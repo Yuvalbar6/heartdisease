3b75a434977a2510585f412a92c672de8e35e4c5
Yuvalbar6 <157469659+Yuvalbar6@users.noreply.github.com>
2024-01-23 11:03:06 +0200

    read from file

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
-print("yuval")
+import pandas as pd
+
+# Assuming your CSV file is named 'your-file.csv'
+file_path = 'framingham.csv'
+
+# Read the CSV file into a pandas DataFrame
+df = pd.read_csv(file_path)
+
+# Now 'df' contains your data, and you can perform operations on it
+print(df.head(10))
